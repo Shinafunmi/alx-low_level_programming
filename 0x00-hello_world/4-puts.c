e824a8ffe7c5151d58676f1ffbc73902697e290c
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-16 07:58:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - program that print a line puts funtion
+ * Return:0 (pass)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
