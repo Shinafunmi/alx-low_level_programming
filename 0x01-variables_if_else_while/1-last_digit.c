4cd6433dccc1d1766bb7bbfa6a1c5537928a65f1
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-19 13:38:48 -0700

    try again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is 0\n", n, m);
 }
 if (m < 6 && m != 0)
 {
-printf("Last digit of %d and is less than 6 and not 0\n", n, m);
+printf("Last digit of %d is and %d is less than 6 and not 0\n", n, m);
 }
 return (0);
 }
