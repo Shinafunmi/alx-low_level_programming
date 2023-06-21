8fb96684887833265f8bfae6b72998c00c2271af
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-21 08:05:12 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; i < 10; j++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+		_putchar(j);
+	}
+	_putchar('\n');
+	}
+}
