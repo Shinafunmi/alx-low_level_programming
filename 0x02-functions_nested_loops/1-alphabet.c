4453b27a06823d8ab5de4b4b3f1b0bda8629fe88
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-21 07:05:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
 
 void print_alphabet(void)
 {
-	int i;
+	char letter;
 
-	for (i = 97; i <= 122; i++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(i);
+		_putchar(letter);
 	}
 	_putchar('\n');
 
