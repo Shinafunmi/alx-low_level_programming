5ad07fe0669dbf4e40b33b0dfd2e28735b23f733
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-21 16:39:49 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - show 1 if the input is 1
+ * lowercase character
+ * 0
+ * @c:  the character in anscii code
+ * Return: return 1 for lowercase character, 0 for the rest;
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+purchar('\n');
+}
