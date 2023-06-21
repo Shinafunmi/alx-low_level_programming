7499b9ccdd900d52517482c56d6653cee81a5478
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-21 06:47:05 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = a; i <= z; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n);
+}
+
