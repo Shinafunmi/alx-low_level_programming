819b47e8a3a6358584604bd2f81f4e69e17fdce3
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-22 13:30:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,12 @@ for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 }
-if (a >= 2; &&b >= 4)
+if (a >= 2 && b >= 4)
 break;
 _putchar(a + 48);
 _putchar(b + 48);
 _putchar(58);
-_putchat(c + 48);
+_putchar(c + 48);
 _putchar(d + 48);
 _putchar('\n');
 }
