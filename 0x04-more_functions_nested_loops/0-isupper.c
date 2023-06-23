1ad29de2b3b94b7c6248b7ed552f213aa42ac83b
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-23 05:52:59 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a number is upper
+ * @c: number to be checked
+ * Return: return 1 for uppercase, return 0 for else
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
