786de4e51c287ca560f1a6e07a3fdc62f61d8f9a
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-23 03:35:10 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two number together
+ * @n1: integer number to be computed
+ * @n2: integer number to be computed
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
