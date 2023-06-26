3a49eac672776b80e508fb77b582173c6071ba95
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-26 15:13:38 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: strings
+ * retunr: length
+ */
+int _strlen(char *s)
+{
+int longi = 0;
+
+while (*s '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
+
