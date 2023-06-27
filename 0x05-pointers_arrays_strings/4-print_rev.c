5807b393af82cf981feccf7c4bf332fad4d4208d
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-27 01:41:38 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function of function
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int o;
+
+while (*s != '\0')
+{
+i++;
+s++;
+}
+s--
+for (o = i; o > 0; o--)
+{
+putchar(*s);
+s--;
+}
+putchar('\n');
+}
