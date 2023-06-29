d8bc4652213aaa808d1c5b694c22ba4603ed8290
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-06-29 05:32:25 -0700

    omo

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenate two string
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+for (i = 0; src[i] != '\0'; i++)
+{
+}
+for (j = 0; dest[i] != '\0'; j++)
+{
+}
+dest[j] += src[i];
+return (dest);
+}
