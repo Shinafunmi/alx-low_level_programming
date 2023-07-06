acc2378e3aa050721d03dffd67b7777974945f91
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-07-05 17:54:30 -0700

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function
+ * @n: number to calculate
+ * Return: resulting square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (actual_sqrt_recursion(n, 0));
+}
