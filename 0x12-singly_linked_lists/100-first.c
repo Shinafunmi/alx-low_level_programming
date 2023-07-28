ad717fb23e5a5e7fa0fa6418a829690b05df5ae3
Shinafunmi <shinafunmiodetuyi22@yahoo.com>
2023-07-28 16:25:01 -0700

    commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void first(void)__attribute__((constructor));
+
+/**
+ * first - print a sentence before main
+ * function is executed
+ * void - void
+ */
+void first(void)
+{
+printf("you're beat! and yet, you must allow,\n");
+printf("i bore my house upon my back!\n");
+}
